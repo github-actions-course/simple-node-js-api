8d0973c4309ae9012bb5c1710d9fc13f4cad6086
alialaa <hello@alialaa.com>
2023-06-24 19:17:21 +0300

    rofl

--- app.js
+++ app.js
@@ -11,15 +11,10 @@ app.use("/assets", express.static(__dirname + "/public"));
 main().catch((err) => console.log(err));
 async function main() {
   await mongoose
-    .connect(
-      `mongodb://${
-        process.env.MONGO_HOSTNAME || "localhost"
-      }:27017/node-api-demo`,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    )
+    .connect(`mongodb://mongo:27017/node-api-demo`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
     .then(() => console.log("Connected."));
 }
 
